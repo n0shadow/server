8e24ef6f0530dc7c9c65a39e956ecabe94153021
n0shadow <221440790+n0shadow@users.noreply.github.com>
2025-07-22 22:54:33 -0300

    update render.py

--- render.py
+++ render.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI, File, UploadFile, HTTPException, Header, status
 from typing import Annotated
 import httpx
 import os
+import requests
+from pydantic import BaseModel
 
 app = FastAPI()
 
 API_KEY_ESTE_SERVIDOR = os.getenv("API_KEY_RENDER")
 URL_SERVIDOR_DESTINO = os.getenv("URL")
-API_KEY_SERVIDOR_DESTINO = os.getenv("API_KEY_DESTINO") or ""
+API_KEY_SERVIDOR_DESTINO = str(os.getenv("API_KEY_DESTINO"))
+T_TOKEN = os.getenv("T_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 @app.post("/file/{username}")
 async def create_upload_file(
@@ -64,6 +68,38 @@ async def create_upload_file(
             )
         finally:
             await file.close()
+
+    
+class ClickData(BaseModel):
+    page: str
+
+@app.post("/click")
+async def click_handler(
+    click_data: ClickData,
+    x_api_key: Annotated[str | None, Header()] = None
+):
+    if x_api_key is None or x_api_key != API_KEY_ESTE_SERVIDOR:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API Key inválida ou ausente para este servidor. Forneça a chave no cabeçalho 'x-api-key'.",
+        )
+    
+    MENSAGEM = f"🚨 Notificação de clique recebida na página: {click_data.page}"
+
+    url = f"https://api.telegram.org/bot{T_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": MENSAGEM
+    }
+
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        return {"message": "Notificação enviada com sucesso!"}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Falha ao enviar notificação."
+        )
 
 @app.get("/")
 async def read_root():
